2ed7bb849241383216097122d88908716a5da6d2
Konstantin Bartosh <rustigillo@gmail.com>
2024-08-13 15:49:46 +0300

    feat: added render for cards, remove & likes for them, handlers for open and closing modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,112 @@
-// @todo: Темплейт карточки
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  }
+];
+const modal = document.querySelector('.popup');
+const modalNewCard = document.querySelector('.popup_type_new-card');
+const modalEditProfile = document.querySelector('.popup_type_edit');
+const modalWithImg = document.querySelector('.popup_type_image')
 
-// @todo: DOM узлы
+const buttonNewCard = document.querySelector('.profile__add-button');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const buttonCloseNewCard = modalNewCard.querySelector('.popup__close');
+const buttonCloseProfile = modalEditProfile.querySelector('.popup__close');
+const buttonCloseImage = modalWithImg.querySelector('.popup__close');
 
-// @todo: Функция создания карточки
+const inputTitleNewCard = modalNewCard.querySelector('.popup__input_type_card-name');
+const inputUrlNewCard = modalNewCard.querySelector('.popup__input_type_url');
 
-// @todo: Функция удаления карточки
+const cardsList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: Вывести карточки на страницу
+function openModalHandler(button, modal) {
+  button.addEventListener('click', function() {
+    modal.classList.add('popup_is-opened');
+  });
+}
+
+function closeModalHandler(button, modal) {
+  button.addEventListener('click', function() {
+    modal.classList.remove('popup_is-opened');
+  });
+}
+
+function handleDeleteCard() {
+  cardsList.addEventListener('click', function(evt) {
+    const target = evt.target;
+
+    if (target.classList.contains('card__delete-button')) {
+      target.closest('.card').remove();
+    }
+  });
+}
+
+function handleLikeCard() {
+  cardsList.addEventListener('click', function(evt) {
+    const target = evt.target;
+
+    if (target.classList.contains('card__like-button')) {
+      target.classList.toggle('card__like-button_is-active');
+    }
+  });
+}
+
+function handleOpenImage() {
+  const captionImage = modalWithImg.querySelector('.popup__caption');
+  const popupImage = modalWithImg.querySelector('.popup__image')
+
+  cardsList.addEventListener('click', function(evt) {
+    if (evt.target.classList.contains('card__image')) {
+      popupImage.src = evt.target.src;
+      captionImage.textContent = evt.target.alt;
+      modalWithImg.classList.add('popup_is-opened');
+    }
+  })
+}
+
+function renderCards(cards) {
+  cards.forEach(card => {
+    const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+  
+    cardItem.querySelector('.card__image').src = card.link;
+    cardItem.querySelector('.card__image').alt = card.name;
+    cardItem.querySelector('.card__title').textContent = card.name;
+
+    cardsList.append(cardItem);
+  });
+}
+
+renderCards(initialCards);
+
+handleDeleteCard();
+handleLikeCard();
+handleOpenImage();
+
+openModalHandler(buttonNewCard, modalNewCard);
+openModalHandler(buttonEditProfile, modalEditProfile);
+
+closeModalHandler(buttonCloseNewCard, modalNewCard);
+closeModalHandler(buttonCloseProfile, modalEditProfile);
+closeModalHandler(buttonCloseImage, modalWithImg);
