c48e40c7e1f0766b56cf8f64c0649fc96b67794a
Konstantin Bartosh <rustigillo@gmail.com>
2024-08-14 13:17:50 +0300

    feat: closing modal by overlay clickind & esc

--- src/index.js
+++ src/index.js
@@ -1,32 +1,6 @@
 import './index.css';
+import { initialCards } from '../src/scripts/constantants';
 
-const initialCards = [
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  }
-];
-const modal = document.querySelector('.popup');
 const modalNewCard = document.querySelector('.popup_type_new-card');
 const modalEditProfile = document.querySelector('.popup_type_edit');
 const modalWithImg = document.querySelector('.popup_type_image')
@@ -112,3 +86,19 @@ openModalHandler(buttonEditProfile, modalEditProfile);
 closeModalHandler(buttonCloseNewCard, modalNewCard);
 closeModalHandler(buttonCloseProfile, modalEditProfile);
 closeModalHandler(buttonCloseImage, modalWithImg);
+
+document.addEventListener('click', function(evt) {
+  const openPopup = document.querySelector('.popup_is-opened');
+
+  if (openPopup && evt.target === openPopup) {
+    openPopup.classList.remove('popup_is-opened');
+  }
+});
+
+document.addEventListener('keydown', function(evt) {
+  const openPopup = document.querySelector('.popup_is-opened');
+
+  if (openPopup && evt.key === 'Escape') {
+    openPopup.classList.remove('popup_is-opened');
+  }
+});
