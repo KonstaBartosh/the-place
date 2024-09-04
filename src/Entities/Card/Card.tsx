4dd6600c7c2183f86d1599b418778c2e3926036d
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-04 19:50:29 +0300

    feat: create entities folder for fsd design & move card and profile inside

---
+++ src/Entities/Card/Card.tsx
@@ -0,0 +1,26 @@
+import { TCard } from '../../Shared/Types/common';
+import styles from './Card.module.css';
+
+type TCardProps = {
+  card: TCard;
+};
+
+const Card = ({ card }: TCardProps) => {
+  const { name, link } = card;
+
+  return (
+    <article className={styles.container}>
+      <img className={styles.image} src={link} alt={name} />
+      <button className={styles.deleteButton} type="button"></button>
+      <div className={styles.description}>
+        <h2 className={styles.title}>{name}</h2>
+        <div className={styles.likeWrapper}>
+          <button className={styles.likeButton} type="button"></button>
+          <span className={styles.likeCounter}></span>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default Card;
---
+++ src/Entities/Profile/Profile.tsx
@@ -0,0 +1,35 @@
+import styles from './Profile.module.css';
+import { TUser } from '../../Shared/Types/common';
+import ProfileSkeleton from './ProfileSkeleton';
+
+type ProfileProps = {
+  user?: TUser | null;
+  isLoading?: boolean;
+};
+
+const Profile = ({ user, isLoading }: ProfileProps) => {
+  if (isLoading || !user) {
+    return <ProfileSkeleton />
+  }
+
+  const { name, about, avatar } = user;
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.imageWrapper}>
+        <img className={styles.image} src={avatar} alt={name} />
+        <div className={styles.overlay}>
+          <div className={styles.overlayContent}></div>
+        </div>
+      </div>
+      <div className={styles.dataWrapper}>
+        <h1 className={styles.title}>{name}</h1>
+        <button className={styles.editButton} type="button" />
+        <p className={styles.occupation}>{about}</p>
+      </div>
+      <button className={styles.addButton} type="button" />
+    </section>
+  );
+};
+
+export default Profile;
