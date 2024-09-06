ea8ea7170b92f5441237d350553bacc9faa73d60
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-06 13:16:18 +0300

    feat: add button ui component

---
+++ src/Shared/Components/UI/Button/Button.tsx
@@ -0,0 +1,23 @@
+import styles from "./Button.module.css";
+
+type TButtonProps = {
+  type: "button" | "submit" | "reset" | undefined;
+  label: string;
+  disabled?: boolean;
+  onClick?: () => void;
+};
+
+const Button = ({ type, label, disabled, onClick }: TButtonProps) => {
+  return (
+    <button
+      className={styles.button}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Button;
