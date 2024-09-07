5467c948d7fda02e733531aa2e9dc3e65c12bad3
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-07 15:58:03 +0300

    feat: integrate modals inside

--- src/Entities/Profile/Profile.tsx
+++ src/Entities/Profile/Profile.tsx
@@ -1,34 +1,65 @@
-import styles from './Profile.module.css';
-import { TUser } from '../../Shared/Types/common';
-import ProfileSkeleton from './ProfileSkeleton';
+import styles from "./Profile.module.css";
+import { useContext, useState } from "react";
+import { UserContext } from "../../Shared/Context/UserContext";
 
-type ProfileProps = {
-  user?: TUser | null;
+import ProfileSkeleton from "./ProfileSkeleton";
+import ProfileModal from "./Modals/ProfileModal";
+import AvatarModal from "./Modals/AvatarModal";
+
+type TProps = {
   isLoading?: boolean;
 };
 
-const Profile = ({ user, isLoading }: ProfileProps) => {
+const Profile = ({ isLoading }: TProps) => {
+  const [profileIsOpen, setProfileOpen] = useState(false);
+  const [userIsOpen, setUserOpen] = useState(false);
+
+  const { user } = useContext(UserContext);
+
   if (isLoading || !user) {
-    return <ProfileSkeleton />
+    return <ProfileSkeleton />;
   }
 
   const { name, about, avatar } = user;
 
   return (
-    <section className={styles.container}>
-      <div className={styles.imageWrapper}>
-        <img className={styles.image} src={avatar} alt={name} />
-        <div className={styles.overlay}>
-          <div className={styles.overlayContent}></div>
+    <>
+      <section className={styles.container}>
+        <div 
+          className={styles.imageWrapper} 
+          onClick={() => setUserOpen(true)}
+          >
+          <img 
+            className={styles.image} 
+            src={avatar} 
+            alt={name} 
+            />
+          <div className={styles.overlay}>
+            <div className={styles.overlayContent}></div>
+          </div>
         </div>
-      </div>
-      <div className={styles.dataWrapper}>
-        <h1 className={styles.title}>{name}</h1>
-        <button className={styles.editButton} type="button" />
-        <p className={styles.occupation}>{about}</p>
-      </div>
-      <button className={styles.addButton} type="button" />
-    </section>
+        <div className={styles.dataWrapper}>
+          <h1 className={styles.title}>{name}</h1>
+          <button
+            className={styles.editButton}
+            onClick={() => setProfileOpen(true)}
+          />
+          <p className={styles.occupation}>{about}</p>
+        </div>
+        <button 
+          className={styles.addButton} 
+          type="button" 
+          />
+      </section>
+      <ProfileModal
+        isOpen={profileIsOpen}
+        onClose={() => setProfileOpen(false)}
+      />
+      <AvatarModal 
+        isOpen={userIsOpen} 
+        onClose={() => setUserOpen(false)} 
+        />
+    </>
   );
 };
 
