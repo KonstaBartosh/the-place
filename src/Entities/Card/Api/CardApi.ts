498c8978a7ad71ab2915bfbe753431c86bc898e4
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-08 21:58:14 +0300

    feat: add types for like handle fetch

--- src/Entities/Card/Api/CardApi.ts
+++ src/Entities/Card/Api/CardApi.ts
@@ -1,11 +1,6 @@
 import { checkResponse } from "../../../App/Api/api";
 import { API_CONFIG } from "../../../Shared/Constants/constants";
 
-type THandleLikeApi = {
-  id: string | undefined;
-  isLiked: boolean | undefined;
-}
-
 async function deleteCardApi(id: string) {
   const res = await fetch(`${API_CONFIG.BASE_URL}/cards/${id}`, {
     method: 'DELETE',
@@ -15,7 +10,10 @@ async function deleteCardApi(id: string) {
   return checkResponse(res);
 }
 
-async function handleLikeApi({ id, isLiked }: THandleLikeApi) {
+async function handleLikeApi(
+  id: string | undefined, 
+  isLiked: boolean | undefined
+  ) {
   const res = await fetch(`${API_CONFIG.BASE_URL}/cards/likes/${id}`, {
     method: isLiked ? 'DELETE' : 'PUT',
     headers: API_CONFIG.headers
