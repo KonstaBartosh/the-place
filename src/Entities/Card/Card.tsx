fae1eb851bb03521cd1a65380da2d0949190de79
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-08 21:53:51 +0300

    feat: add fn for delete and likes the card

--- src/Entities/Card/Card.tsx
+++ src/Entities/Card/Card.tsx
@@ -1,22 +1,78 @@
-import { TCard } from '../../Shared/Types/common';
-import styles from './Card.module.css';
+import styles from "./Card.module.css";
+import { useCallback, useContext, useMemo } from "react";
+import { UserContext } from "../../Shared/Context/UserContext";
+import { CardsContext } from "../../Shared/Context/CardsContext";
+import { TCard } from "../../Shared/Types/common";
+import { deleteCardApi, handleLikeApi } from "./Api/CardApi";
 
-type TCardProps = {
+type TProps = {
   card: TCard;
 };
 
-const Card = ({ card }: TCardProps) => {
-  const { name, link } = card;
+const Card = ({ card }: TProps) => {
+  const { cards, setCards } = useContext(CardsContext);
+  const { user } = useContext(UserContext);
+
+  const { _id, name, link, owner, likes } = card;
+  console.log("Card component rendered");
+
+  const isUserOwner = useMemo(
+    () => owner?._id === user?._id, [owner, user]);
+
+  const likeCounter = useMemo(
+    () => likes?.length || null, [likes]);
+
+  const isLiked = useMemo(
+    () => likes?.some((like) => like._id === user?._id), [likes, user]);
+
+
+  const handleDelete = useCallback(async () => {
+    if (isUserOwner && _id) {
+      try {
+        await deleteCardApi(_id);
+        setCards(cards.filter((item) => item._id !== card._id));
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [_id, isUserOwner, setCards, cards]);
+
+  const handleLike = useCallback(async () => {
+    try {
+      const resCard = await handleLikeApi(_id, isLiked);
+      setCards(
+        cards.map((card) => (card._id === resCard._id ? resCard : card))
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }, [_id, isLiked, setCards, cards]);
+
+  const likeButtonClass = `${styles.likeButton} ${
+    isLiked && styles.likeButtonIsActive
+  }`;
 
   return (
     <article className={styles.container}>
       <img className={styles.image} src={link} alt={name} />
-      <button className={styles.deleteButton} type="button"></button>
+      {isUserOwner && (
+        <button 
+          className={styles.deleteButton}
+          aria-label="Delete card"
+          onClick={handleDelete} 
+          />
+      )}
       <div className={styles.description}>
         <h2 className={styles.title}>{name}</h2>
         <div className={styles.likeWrapper}>
-          <button className={styles.likeButton} type="button"></button>
-          <span className={styles.likeCounter}></span>
+          <button 
+            className={likeButtonClass} 
+            aria-label={isLiked ? "Unlike card" : "Like card"}
+            onClick={handleLike} 
+            />
+          <span className={styles.likeCounter}>
+            {likeCounter}
+          </span>
         </div>
       </div>
     </article>
