a464c19709b5387ad2daf8b6953d6c34d8f490e4
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-17 16:01:18 +0300

    feat: add navigation button

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,11 +1,37 @@
+import { Link, useLocation } from 'react-router-dom';
 import ThemeToggle from '../../features/themeToggle/ui/ThemeToggle/ThemeToggle';
 import styles from './Header.module.css';
 
 const Header = (): React.ReactElement => {
+  const location = useLocation();
+
+  const isHome = location.pathname === '/';
+
+  const HomeLink = () => (
+    <Link
+      to="/"
+      className={styles.link}
+    >
+      Home
+    </Link>
+  );
+
+  const LoginLink = () => (
+    <Link
+      to="/login"
+      className={styles.link}
+    >
+      Login
+    </Link>
+  );
+
   return (
     <header className={styles.container}>
       <h1 className={styles.title}>The Place</h1>
-      <ThemeToggle />
+      <div className={styles.options}>
+        <ThemeToggle />
+        {!isHome ? <HomeLink /> : <LoginLink />}
+      </div>
     </header>
   );
 };
