f1e2dd48048d85c67ecef7e6c855e92a4c73f32b
Konstantin Bartosh <rustigillo@gmail.com>
2024-09-22 21:22:41 +0300

    chore: remove log

--- src/features/ProtectedRoute/ProtectedRoute.tsx
+++ src/features/ProtectedRoute/ProtectedRoute.tsx
@@ -3,8 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoute = () => {
   const isToken = localStorage.getItem('token') ? true : false;
 
-  console.log(isToken);
-
   return isToken ? <Navigate to='/' /> : <Outlet />;
 };
 
