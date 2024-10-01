a6db0ae25838fd867c8f9f4037bed29a4b5693eb
Konstantin Bartosh <rustigillo@gmail.com>
2024-10-01 17:18:40 +0300

    feat: create entry point

---
+++ src/features/auth/index.ts
@@ -0,0 +1,7 @@
+import AuthForm from './ui/AuthForm/AuthForm';
+import * as registerModel from './model/useRegister';
+import * as loginModel from './model/useLogin';
+import { TAuthData } from './types/types';
+
+export { AuthForm, registerModel, loginModel };
+export type { TAuthData };
