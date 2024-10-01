00d1ba119c9648237e145086c0fb6abe2bcff0f8
Konstantin Bartosh <rustigillo@gmail.com>
2024-10-01 17:05:10 +0300

    chore: moved away auth api calls

--- src/App/api/api.ts
+++ src/App/api/api.ts
@@ -1,4 +1,4 @@
-import { API_CONFIG, AUTH_URL } from '../constants/constants';
+import { API_CONFIG } from '../constants/constants';
 import { TUser } from '../types/common';
 
 export function checkResponse(res: Response) {
@@ -30,7 +30,10 @@ async function deleteCardApi(id: string) {
   return checkResponse(res);
 }
 
-async function handleLikeApi(id: string | undefined, isLiked: boolean | undefined) {
+async function handleLikeApi(
+  id: string | undefined,
+  isLiked: boolean | undefined
+) {
   const res = await fetch(`${API_CONFIG.BASE_URL}/cards/likes/${id}`, {
     method: isLiked ? 'DELETE' : 'PUT',
     headers: API_CONFIG.headers,
@@ -81,48 +84,6 @@ async function updateAvatarApi(user: TUser) {
   return checkResponse(res);
 }
 
-const registerApi = async (data: any) => {
-  const { email, password } = data;
-
-  try {
-    const registeredUser = await fetch(AUTH_URL + '/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const result = await checkResponse(registeredUser);
-
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const authorizeApi = async (data: any) => {
-  const { email, password } = data;
-
-  try {
-    const loggedUser = await fetch(AUTH_URL + '/signin', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const result = await checkResponse(loggedUser);
-
-    return result;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
 export {
   fetchCards,
   fetchUser,
@@ -131,6 +92,4 @@ export {
   postCardApi,
   updateUserApi,
   updateAvatarApi,
-  registerApi,
-  authorizeApi,
 };
