89b27c713810fafa1172f29b20e4129591369d34
Konstantin Bartosh <rustigillo@gmail.com>
2024-10-09 13:43:52 +0300

    feat: add home page

--- src/App/router/AppRouter.tsx
+++ src/App/router/AppRouter.tsx
@@ -1,23 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import { CardsList, ProtectedRoute } from '../../features';
-import { Profile } from '../../widgets';
-import { NotFoundPage, LoginPage, RegisterPage } from '../../pages';
+import { ProtectedRoute } from '../../features';
+import { NotFoundPage, LoginPage, RegisterPage, HomePage } from '../../pages';
 
 const AppRouter = () => {
-  const HomePage = () => {
-    return (
-      <>
-        <Profile />
-        <CardsList />
-      </>
-    );
-  };
-
   return (
     <main style={{ flexGrow: 1 }}>
       <Routes>
         <Route
-          path="/"
+          path="/feed"
           element={<HomePage />}
         />
         <Route
