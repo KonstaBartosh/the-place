b118010fe423b0cb22a711a1377423d76d4e5f56
Konstantin Bartosh <rustigillo@gmail.com>
2024-10-11 22:20:09 +0300

    fix: move conditional render from app that depends on the route

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -1,14 +1,20 @@
+import { useLocation } from 'react-router-dom';
 import styles from './Footer.module.css';
 
 const Footer = () => {
   const year = new Date().getFullYear();
 
+  const { pathname } = useLocation();
+  const isHomePage = pathname === '/';
+
   return (
-    <footer className={styles.container}>
-      <p className={styles.copyright}>
-        &copy; {year} The Place. All rights reserved.
-      </p>
-    </footer>
+    isHomePage && (
+      <footer className={styles.container}>
+        <p className={styles.copyright}>
+          &copy; {year} The Place. All rights reserved.
+        </p>
+      </footer>
+    )
   );
 };
 
